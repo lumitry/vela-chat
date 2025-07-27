407bf75583bf612016ccf4c1bcf7bf3ac26c5f04
lumitry <53492381+lumitry@users.noreply.github.com>
2025-07-27 13:30:44 -0400

    fix favicon in sidebar not displaying in dev on different machine than host

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -685,7 +685,6 @@
 				<div class="flex items-center">
 					<div class="self-center mx-1.5">
 						<img
-							crossorigin="anonymous"
 							src="{WEBUI_BASE_URL}/static/favicon.png"
 							class=" size-5 -translate-x-1.5 rounded-full"
 							alt="logo"
