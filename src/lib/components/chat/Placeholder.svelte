f45d75a60f6af50c40c72c66c09fb56595596dbf
lumitry <53492381+lumitry@users.noreply.github.com>
2025-08-01 09:50:41 -0400

    Fix another crossorigin=anonymous dev-only issue (I'm like 70% sure these are pointless since we're not in a Canvas)

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -121,7 +121,6 @@
 									}}
 								>
 									<img
-										crossorigin="anonymous"
 										src={model?.info?.meta?.profile_image_url ??
 											($i18n.language === 'dg-DG'
 												? `/doge.png`
